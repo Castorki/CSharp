3d5f14d04430314f2d725735bcb6dd33fa45f648
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-04 15:42:09 +0300

    added Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] mas = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+if (mas[0] <= mas.Length)
+{
+    Console.WriteLine(mas[0]);
+}
+if (mas[1] <= mas.Length)
+{
+    Console.WriteLine(mas[1]);
+}
+if (mas[2] <= mas.Length)
+{
+    Console.WriteLine(mas[2]);
+}
+if (mas[3] <= mas.Length)
+{
+    Console.WriteLine(mas[3]);
+}
+if (mas[4] <= mas.Length)
+{
+    Console.WriteLine(mas[4]);
+}
+if (mas[5] <= mas.Length)
+{
+    Console.WriteLine(mas[5]);
+}
+if (mas[6] <= mas.Length)
+{
+    Console.WriteLine(mas[6]);
+}
+if (mas[7] <= mas.Length)
+{
+    Console.WriteLine(mas[7]);
+}
+if (mas[8] <= mas.Length)
+{
+    Console.WriteLine(mas[8]);
+}
+if (mas[9] <= mas.Length)
+{
+    Console.WriteLine(mas[9]);
+}
