2414cf42c8b11c27aa32439d6ac588f2d50cf2c9
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-04 13:51:20 +0300

    added a few variables

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine (value);
+
+double a = 5.55;
+char b = '!';
+bool c = true;
+Console.WriteLine (a);
+Console.WriteLine (b);
+Console.WriteLine (c);
