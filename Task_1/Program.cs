299b51c886d87a1ff4bfc6c9eed1d672caeb1f70
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-07 09:25:48 +0300

    Fol

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,11 +1,26 @@
 ﻿//Console.WriteLine("Hello, World!");
 
-int value = 100;
-Console.WriteLine (value);
+// int value = 100;
+// Console.WriteLine (value);
 
-double a = 5.55;
-char b = '!';
-bool c = true;
-Console.WriteLine (a);
-Console.WriteLine (b);
-Console.WriteLine (c);
+// double a = 5.55;
+// char b = '!';
+// bool c = true;
+// Console.WriteLine (a);
+// Console.WriteLine (b);
+// Console.WriteLine (c);
+
+
+Console.Write("Input first number; ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input seconde number; ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    System.Console.WriteLine(a);
+}
+else 
+{
+    System.Console.WriteLine(b);
+}
