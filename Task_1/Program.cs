4b59eab4c781e44b8f7a78643297b0bc45429834
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-13 20:44:15 +0300

    nek

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,26 +1,44 @@
-﻿//Console.WriteLine("Hello, World!");
+﻿// //Console.WriteLine("Hello, World!");
 
-// int value = 100;
-// Console.WriteLine (value);
+// // int value = 100;
+// // Console.WriteLine (value);
 
-// double a = 5.55;
-// char b = '!';
-// bool c = true;
-// Console.WriteLine (a);
-// Console.WriteLine (b);
-// Console.WriteLine (c);
+// // double a = 5.55;
+// // char b = '!';
+// // bool c = true;
+// // Console.WriteLine (a);
+// // Console.WriteLine (b);
+// // Console.WriteLine (c);
 
+
+// Console.Write("Input first number; ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input seconde number; ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a>b)
+// {
+//     System.Console.WriteLine(a);
+// }
+// else 
+// {
+//     System.Console.WriteLine(b);
+// }
 
 Console.Write("Input first number; ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input seconde number; ");
+Console.Write("Input second number; ");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number; ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
 
-if (a>b)
+if (max<b)
+  {
+    max = b;
+  }
+if (max<c)
 {
-    System.Console.WriteLine(a);
+  max = c;
 }
-else 
-{
-    System.Console.WriteLine(b);
-}
+Console.WriteLine(max);
